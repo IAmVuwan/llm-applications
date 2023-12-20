affb14282ec976b434f45aba97e077e03ee4932f
Bhuwan Prasad Upadhyay <bhuwan.upadhyay49@gmail.com>
2023-12-19 20:55:45 -0600

    SYNC

--- notebooks/pg_vector_utils.py
+++ notebooks/pg_vector_utils.py
@@ -54,3 +54,22 @@ def query_number_of_rows():
             cursor.execute("SELECT count(*) FROM document;")
             num_rows = cursor.fetchone()[0]
             print(f"Number of rows in 'document' table: {num_rows}")
+
+
+def save_index():
+    # Save index
+    if os.path.exists(sql_dump_fp):
+        os.remove(sql_dump_fp)
+
+    os.makedirs(os.path.dirname(sql_dump_fp), exist_ok=True)
+    open(sql_dump_fp, 'a').close()
+
+    # Connect to PostgreSQL and save the dump
+    try:
+        with psycopg2.connect(db_connection_string) as connection:
+            with connection.cursor() as cursor:
+                with open(sql_dump_fp, "w") as sql_dump_file:
+                    cursor.copy_to(sql_dump_file, "document", null="")
+        print(f"Index saved to {sql_dump_fp}")
+    except psycopg2.Error as e:
+        print(f"Error: Unable to save index to {sql_dump_fp}. {e}")
